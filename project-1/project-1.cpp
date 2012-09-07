6267936ad4ea68e91d11220f4594981dc63da6e4
Kwajo <vivoon29@gmail.com>
2012-09-06 22:05:04 -0600

    you killed us

---
+++ project-1/project-1.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv )
+{
+ 
+ cout<<"Frank Gadri\n";
+ cout<<"CNS 3060\n";
+ 
+ for(int i =1; i< argc; i++)
+{
+ 
+	cout<< argv[i]<<"\n";
+}
+ return 0;
+ 
+}
+
+
+
